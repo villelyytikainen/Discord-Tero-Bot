383ceea1c3ce830b5b8d7b945f5adc1c1cdec615
Ville Lyytikäinen <corpsefin@gmail.com>
2021-08-23 06:47:07 +0300

    updated

--- index.js
+++ index.js
@@ -1,59 +1,23 @@
-(function(){
-    const Discord = require('discord.js');
-    const {prefix, token} = require('./config.json');
-    const deleteMessages = require('./commands/deleteMessages');
-    const getPrice = require('./commands/getPrice');
+require('dotenv').config();
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client();
 
-    const client = new Discord.Client();
-    const answers = ['mitä?', 'no mitä?!', 'mitä helvettiä?!', 
-                    'no mitä tahdot?!', 'oliko asiaa?', 'kerro?'];
+client.once('ready', ()=>{
+    
+    console.log('bot connected');
+});
 
-    client.once('ready', ()=>{
-        console.log('bot connected');
-    });
-
-    client.on('message', async (msg)=>{
-        let args = msg.content.slice(prefix.length).trim().split(/ +/);
-        if(!msg.content.includes('"'))
-            args = msg.content.slice(prefix.length).trim();
-            
-        const command = args.shift().toLowerCase();
-
-        switch(command){
-            case 'tuhoa':
-                const amount = parseInt(args[0]) + 1;
-                if (!amount)
-                    msg.channel.send('anna viestien lukumäärä jotka poistaa');
-                if (amount > 0 && amount < 100) {
-                    deleteMessages(msg, amount);
-                }
-                break;
-            case 'jimms':
-                const product = args[0];
-                const count = args[1] || 1;
-                // count != Number ? msg.channel.send('anna saatana numero!') : count;
-                getPrice(product, count).then(data=>{
-                    console.table(data.length);
-                    for(let i=0; i<data.length; i++){
-                        msg.channel.send(JSON.stringify(`${[i+1]}. *${data[i].name}* **${data[i].price}**`, null, 4).replace('"', ''));
-                    }
-                    //msg.channel.send(JSON.stringify(data, null, 4));
-                }).catch(err =>{
-                    console.log('error', err);
-                })
-                break;
+client.on('message', (msg) =>{
+    let args = msg.content.slice(prefix.length).trim().split(/ +/);
+    if(msg.content[0] === prefix){
+        if(args[0] === 'steam'){
+            msg.reply(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_TOKEN}&steamids=76561197960435530`)
         }
-        if(msg.content.startsWith(prefix) && !command){
-            msg.channel.send(`${msg.author.username}, ${answers[(Math.floor(Math.random()*answers.length))]}`);
-        }
-        else if(!msg.content.startsWith(prefix) || msg.author.bot)
-            return;
-
-        if(msg.content.includes('3080')){
-            msg.channel.send('https://i.ytimg.com/vi/BVN9k24KtfM/maxresdefault.jpg');
-        }
-    })
+    }
+        
+})
 
 
-    client.login(token);
-})();
+client.login(token);
