6438527fa86ed4cbeac6825272859a61bfcb7051
Ville Lyytikäinen <corpsefin@gmail.com>
2021-08-23 08:20:16 +0300

    changed steam link

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on('message', (msg) =>{
     let args = msg.content.slice(prefix.length).trim().split(/ +/);
     if(msg.content[0] === prefix){
         if(args[0] === 'steam'){
-            msg.reply(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_TOKEN}&steamids=76561197960435530`)
+            msg.reply(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_TOKEN}&personaname=${args[1]}`)
         }
     }
         
